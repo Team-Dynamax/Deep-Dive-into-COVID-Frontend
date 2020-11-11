b3c4ea9887ac2ae50af0753d3f4b95edc045ee63
JoshuaMRampersad <joshua.rampersad@my.uwi.edu>
2020-11-11 07:17:16 -0400

    small viz changes and product page work in  progress

--- src/containers/Product/Product.js
+++ src/containers/Product/Product.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import './Product.css';
+import team from "../DashBoard/rawpixel-669610-unsplash-1.webp";
 
 
 export const Product = () => {
     return (
         <div>
-            <div className='Details'>
-                404
-                <p>Hello</p>
-                </div>  
+            <div className='Details'style = {{ backgroundImage: `url(${team})` }}>
+                
+                <p><div className='Coviz'>Coviz built by Team Dynamax</div> </p> 
+                <br></br>
+                <p><div className='Managers'>Managers<p>.</p><p>.</p><p>.</p></div>
+                <br></br>
+                <div className ='Frontend'>The Frontend team<p>.</p><p>.</p><p>.</p></div>
+                <br></br>
+                <div   className='Backend'>The Backend team<p>.</p><p>.</p><p>.</p></div>
+                <br></br>
+                <div  className='Testing'>The Testing team<p>.</p><p>.</p><p>.</p></div>
+                </p> 
+                   
+        </div>
         </div>
     )
 }
