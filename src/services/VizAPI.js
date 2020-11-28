1145f2d1a41dbbef688e575225350229fe7b728e
SamuelBissessar <lee.bissessar@my.uwi.edu>
2020-11-27 23:10:57 -0400

    cleaned code

--- src/services/VizAPI.js
+++ src/services/VizAPI.js
@@ -1,29 +1,22 @@
-import React from 'react'
-import Plotly from 'plotly.js/dist/plotly-cartesian';
-import createPlotlyComponent from 'react-plotly.js/factory';
-import {ReactComponent as Spin} from '../styles/icons/fillsync-24px.svg';
+import React from "react";
+import Plotly from "plotly.js/dist/plotly-cartesian";
+import createPlotlyComponent from "react-plotly.js/factory";
+import { ReactComponent as Spin } from "../styles/icons/fillsync-24px.svg";
 
 const Plot = createPlotlyComponent(Plotly);
 
-const VizAPI = ({data, layout}) => {
+const VizAPI = ({ data, layout }) => {
+  const Load = ({ valid }) => {
+    if (valid === null) {
+      return <Spin className="spinner" alt="" />;
+    } else return <Plot data={data} layout={layout} />;
+  };
 
-    const Load = ({valid}) => {
-        
-        console.log(valid)
+  return (
+    <div className="center">
+      <Load valid={layout} />
+    </div>
+  );
+};
 
-        if (valid === null) {
-            return <Spin className="spinner" alt=""/>
-        }
-
-        else return <Plot data={data} layout={layout} /> 
-
-    }
-
-    return (
-        <div className="center">
-            <Load valid={layout} />
-        </div>
-    )
-}
-
-export default VizAPI
+export default VizAPI;
